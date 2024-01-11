1ba104b28d67bd56ba5f6370a0f0514b32dcdb46
Kelechi Chijioke <kingsleychijioke824@gmail.com>
2024-01-11 13:03:07 +0100

    This a working version of the former code

--- Calculator.js
+++ Calculator.js
@@ -1,87 +1,43 @@
-class Calculator {
-    add(x, y) {
-        return x + y;
-    }
+function add(x, y) {
+    return x + y;
+};
 
-    subtract(x, y) {
-        return x - y;
-    }
+function subtract(x, y) {
+    return x - y;
+}
 
-    multiply(x, y) {
-        return x * y;
-    }
+function multiply(x, y) {
+    return x * y;
+}
 
-    divide(x, y) {
-        if (y !== 0) {
-            return x / y;} 
-        else {
-            return 'Cannot work';
-        }
-    }
+function divide(x, y) {
+    return x / y;
+}
 
-    squareRoot(x) {
-        if (x >= 0) {
-            return Math.sqrt(x);
-        } 
-        else {
-            return 'Square root of a negative number is undefined';
-        }
-    }
+function absoluteValue(x) {
+    return Math.abs(x);
+}
 
-    absoluteValue(x) {
-        if (x >= 0) {
-            return Math.abs(x);
-        }
-        else {
-            return 'Absolute of a negative number is undefined';
-        }
-    }
+function cosine(x) {
+    return Math.cos(x);
+}
 
-    ceilingValue(x) {
-        if (x >= 0) {
-            return Math.ceil(x);
-        }
-        else {
-            return 'Ceiling value of a negative number is undefined';
-        }
-    }
+function floorValue(x) {
+    return Math.floor(x);
+}
 
-    floorValue(x) {
-        if (x >= 0) {
-            return Math.floor(x);
-        }
-        else {
-            return 'Floor value of a negative number is undefined';
-        }
-    }
+function ceilingValue(x) {
+    return Math.ceiling(x);
+}
 
-    roundValue(x) {
-        if (x >= 0) {
-            return Math.round(x);
-        }
-        else {
-            return 'Round value of a negative number is undefined';
-        }
-    }
-
-    cosine(x) {
-        if (x >= 0) {
-            return Math.cos(x);
-        }
-        else {
-            return 'Cos of a negative number is undefined';
-        }
-    }
+function tangent(x) {
+    return Math.tan(x);
 }
 
 
 
-console.log(`Addition: ${calculator.add(num1, num2)}`);
-console.log(`Subtraction: ${calculator.subtract(num1, num2)}`);
-console.log(`Multiplication: ${calculator.multiply(num1, num2)}`);
-console.log(`Division: ${calculator.divide(num1, num2)}`);
-console.log(`Square Root: ${calculator.squareRoot(num1)}`);
-console.log(`Ceiling Value: ${calculator.ceilingValue(num1)}`);
-console.log(`Floor Value: ${calculator.floorValue(num1)}`);
-console.log(`Round Number: ${calculator.roundValue(num1)}`);
-console.log(`Cosine: ${calculator.cosine(num1)}`);
+
+
+// Sample implementation
+let result = floorValue(50.345678999);
+console.log (result);
